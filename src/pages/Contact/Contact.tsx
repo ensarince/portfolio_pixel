c8c9ad467f0426599543f84f464311d02ca14a8e
ensar <32306029+ensarince@users.noreply.github.com>
2023-03-05 11:14:25 +0100

    blog page skills page development continues1

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -5,9 +5,13 @@ type Props = {}
 
 export default function Contact({}: Props) {
   return (
-    <div>
+    <>
       <Header />
-      Contact
+
+      <div style={{display:"flex", height:"80vh", justifyContent:"center", alignItems:"center"}}>
+
+        <img style={{width:"45%", borderRadius:"3rem", border:"1px solid #A9C5B9", padding:"2rem"}} src="https://giffiles.alphacoders.com/158/158667.gif" alt="" />
       </div>
+    </>
   )
 }
