7c851510ccadd25817f98447bee3f26fa367a5a2
ensar <32306029+ensarince@users.noreply.github.com>
2023-04-22 17:15:56 +0200

    post filtering fixed

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -13,42 +13,46 @@ type Props = {
 
 export default function Blog({posts}: Props) {
 
-  //const [filteredPosts, setFilteredPosts] = useState<BlogPost[] | undefined>([]);
+  const [filteredPosts, setFilteredPosts] = useState<BlogPost[] | undefined>([]);
+  const [categories, setCategories] = useState<Category[] | any>([]);
   const navigate = useNavigate()
 
-/*   const filterPosts = (preferredCategory: string | undefined): void => {
+  const filterPosts = (preferredCategory: string | undefined): void => {
     const filtered = posts?.filter(post => post.categories[0]._ref === preferredCategory);
     setFilteredPosts(filtered);
-  }; */
+  };
 
-  //needed to show sanity images
   const builder = imageUrlBuilder(sanityClient)
   
   function urlFor(source: SanityImageSource) {
     return builder.image(source)
   }
 
-  //console.log(filteredPosts)
+  useEffect(() => {
+    const categoryRefs = posts?.map((post) => post.categories[0]._ref);
+    if (categoryRefs) {
+      Promise.all(categoryRefs.map((ref) => sanityClient.fetch(`*[_id == "${ref}"][0].title`)))
+        .then((titles) => setCategories(titles.map((title, index) => ({_id: categoryRefs[index], title}))));
+    }
+  }, [posts]);
 
+  useEffect(() => {
+    setFilteredPosts(posts)
+  }, [posts])
+  
   return (
     <>
       <Header />
-{/*     <div className={styles.buttonContainer}>
+    <div className={styles.buttonContainer}>
       <button onClick={() => filterPosts('91e45e40-f3a0-4488-85e9-fb87eafac059')} className={styles.categoryButton}>Climbing</button>
       <button onClick={() => filterPosts('807d499a-f06d-4ef5-a1e9-c4413d9be7eb')} className={styles.categoryButton}>Coding</button>
       <button onClick={() => filterPosts("85ab035c-ede8-4700-8c9b-c9eddf6199d5")} className={styles.categoryButton}>Other</button>
-    </div> */}
+      {(filteredPosts?.length !== posts?.length) && <button onClick={() => setFilteredPosts(posts)} style={{color:"#cd5e56"}} className={styles.categoryButton}>Reset</button>}
+    </div>
 
-      <div className={styles.container}>
-        {posts?.map((item, index) => {
-          const categoryRef = item.categories[0];
-          const [category, setCategory] = useState("")
-
-          useEffect(() => {
-            sanityClient
-              .fetch(`*[_id == "${categoryRef._ref}"][0].title`)
-              .then((data) => setCategory(data));
-          }, [categoryRef]);
+<div className={styles.container}>
+        {filteredPosts?.map((item, index) => {
+          const category = categories.find((category: any) => category._id === item.categories[0]._ref)?.title;
 
           return (
             <div onClick={() => navigate(`/blog/${item._id}`)} key={item._id} className={styles.post}>
@@ -59,9 +63,6 @@ export default function Blog({posts}: Props) {
               />
               <h3 className={styles.postTitle}>{item.title}</h3>
               <h4 className={styles.postSummary}>{item.summary}</h4>
-{/*               <p className={styles.postText}>
-                {item.body}
-              </p> */}
               <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"1rem"}}>
                 <div className={styles.postCategory}><h4>Category</h4>: {category}</div>
                 <p className={styles.postDate}>{new Date(item._createdAt).toLocaleDateString('en-GB')}</p>
